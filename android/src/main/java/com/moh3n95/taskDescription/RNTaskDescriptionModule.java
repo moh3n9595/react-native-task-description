06e94a90542aa732e646d660da1cdf634be14225
Mohsen <mohsenando@gmail.com>
2020-06-02 11:48:21 +0430

    Fix onExit Bug

--- android/src/main/java/com/moh3n95/taskDescription/RNTaskDescriptionModule.java
+++ android/src/main/java/com/moh3n95/taskDescription/RNTaskDescriptionModule.java
@@ -5,15 +5,19 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.UiThreadUtil;
 
 import android.app.ActivityManager;
 import android.os.Build;
 import android.graphics.Color;
 import android.content.Context;
+import android.app.Activity;
 
 public class RNTaskDescriptionModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
+  private String _color = null;
+  private String _label = null;
 
   public RNTaskDescriptionModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -27,19 +31,31 @@ public class RNTaskDescriptionModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void setHeader(String label, String color) {  
-    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-			ActivityManager activityManager = (ActivityManager) getCurrentActivity().getSystemService(Context.ACTIVITY_SERVICE);
-			for (ActivityManager.AppTask appTask : activityManager.getAppTasks()) {
-				if(appTask.getTaskInfo().id == getCurrentActivity().getTaskId()) {
-					ActivityManager.TaskDescription description = appTask.getTaskInfo().taskDescription;
-					getCurrentActivity().setTaskDescription(new ActivityManager.TaskDescription(
-            (label != null)? label : description.getLabel(), 
-            description.getIcon(), 
-            (color != null)? Color.parseColor(color) : description.getPrimaryColor()
-          ));
-				}
-			}
-		}
+    final Activity activity = getCurrentActivity();
+
+    if (activity == null) return;
+
+    _label = label;
+    _color = color;
+
+    UiThreadUtil.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+          ActivityManager activityManager = (ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE);
+          for (ActivityManager.AppTask appTask : activityManager.getAppTasks()) {
+            if(appTask.getTaskInfo().id == activity.getTaskId()) {
+              ActivityManager.TaskDescription description = appTask.getTaskInfo().taskDescription;
+              activity.setTaskDescription(new ActivityManager.TaskDescription(
+                (_label != null)? _label : description.getLabel(), 
+                description.getIcon(), 
+                (_color != null)? Color.parseColor(_color) : description.getPrimaryColor()
+              ));
+            }
+          }
+		    }
+      }
+    });
   }
 
 }
